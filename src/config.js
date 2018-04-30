85b82ba06a600be305728e3585f576149988aeca
ohad <master.gefen@gmail.com>
2018-04-30 10:39:26 +0300

    fixing config

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 const config = {
-    host: '127.0.0.1',
-    // host: process.env.REDIS_HOST || '172.17.0.7',
+    // host: '127.0.0.1',
+    host: process.env.REDIS_HOST || '172.17.0.7',
     port: process.env.REDIS_PORT || 6379,
     KEY: process.env.REDIS_KEY || 'onAir1',
     SIMULATION_KEY:  process.env.REDIS_SIMULATION_KEY ||'jfk2',
     CHANNEL: process.env.CHANNEL || 'messageAdded',
     NUMBER_OF_LOOPS: Infinity , // Infinity
-    START: 0,
-    END :100,
+    START: process.env.START || 0,
+    END :process.env.END ||-1,
     updateIntervalMs: 20000,
     setIntervalMs: 15000,
     setTimeoutMs: 5000,
